c947ca02781e966596aeb2c1db65175720ad948c
Luwangel <adrien.amoros@gmail.com>
2020-04-01 12:00:59 +0200

    Finish the renaming from google-sheet to google-sheets

--- packages/demo/config/config-dev.js
+++ packages/demo/config/config-dev.js
@@ -2,7 +2,7 @@
     console.log('Loading dev config');
 
     window.CONFIG = {
-        PAGE_TITLE: 'Demo ra-data-google-sheet',
+        PAGE_TITLE: 'Demo ra-data-google-sheets',
         API_KEY: 'AIzaSyAg7KMhv203LygtsIjSH4w_Qm7vRpeyAEA',
     };
 })();
