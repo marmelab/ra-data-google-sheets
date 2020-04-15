80a68b40e268fd9858c09976848b358a490dc4c0
Luwangel <adrien.amoros@gmail.com>
2020-04-15 14:13:34 +0200

    Add a create verb in google sheets data provider

--- packages/ra-data-google-sheets/src/googleSheetsDataProvider.js
+++ packages/ra-data-google-sheets/src/googleSheetsDataProvider.js
@@ -1,62 +1,78 @@
-import { processSheet } from './processGoogleSheetsData';
+import { processSheet } from './formatDataForReactAdmin';
+import { processForm } from './formatDataForGoogleSheets';
 
 const GOOGLE_SHEETS_DOC_ID = '1cagTLWhyPnlFpgCcu19W4O0wBCzXxEyDb5yBK_jg-PU';
 
+const getResourceRows = async resource => {
+    const response = await window.gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: GOOGLE_SHEETS_DOC_ID,
+        range: `${resource}!A:Z`,
+    });
+
+    return processSheet(response.result.values);
+};
+
+const getResourceHeaders = async resource => {
+    const response = await window.gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: GOOGLE_SHEETS_DOC_ID,
+        range: `${resource}!A1:Z1`,
+    });
+
+    return processSheet(response.result.values).headers;
+};
+
+const countResourceRows = async resource => {
+    const response = await window.gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: GOOGLE_SHEETS_DOC_ID,
+        range: `${resource}!A1:A1000`,
+    });
+
+    return processSheet(response.result.values).total;
+};
+
+const appendResourceRows = async (resource, rows) => {
+    return await window.gapi.client.sheets.spreadsheets.values.append({
+        spreadsheetId: GOOGLE_SHEETS_DOC_ID,
+        range: `${resource}!A1`,
+        valueInputOption: 'RAW',
+        resource: {
+            values: rows,
+        },
+    });
+};
+
 export const googleSheetsDataProvider = () => {
     return {
-        create: async (resource, { data }) => {
-            const response = await window.gapi.client.sheets.spreadsheets.values.get(
-                {
-                    spreadsheetId: GOOGLE_SHEETS_DOC_ID,
-                    range: `${resource}!A1:Z1`,
-                }
-            );
+        create: async (resource, { data: formData }) => {
+            const headers = await getResourceHeaders(resource);
+            const total = await countResourceRows(resource);
 
-            const headers = response.result.values[0];
+            const newId = total + 1; // Auto-Increment
+            const values = processForm(headers, newId, formData);
 
-            const values = headers.map(header => {
-                if (header === 'id') {
-                    return '';
-                }
-                return data[header];
-            });
+            await appendResourceRows(resource, values);
 
-            await window.gapi.client.sheets.spreadsheets.values
-                .append({
-                    spreadsheetId: GOOGLE_SHEETS_DOC_ID,
-                    range: `${resource}!A1`,
-                    resource: {
-                        values,
-                    },
-                })
-                .then(response => {
-                    var result = response.result;
-                    console.log(
-                        `${result.updates.updatedCells} cells appended.`
-                    );
-                });
+            return {
+                data: { ...formData, id: newId },
+            };
         },
         delete: () => Promise.resolve(null),
         deleteMany: () => Promise.resolve(null),
         getList: async resource => {
-            const response = await window.gapi.client.sheets.spreadsheets.values.get(
-                {
-                    spreadsheetId: GOOGLE_SHEETS_DOC_ID,
-                    range: `${resource}!A:Z`,
-                }
-            );
+            const result = await getResourceRows(resource);
 
-            return processSheet(response.result.values);
+            return {
+                data: result.data,
+                total: result.total,
+            };
         },
         getMany: async resource => {
-            const response = await window.gapi.client.sheets.spreadsheets.values.get(
-                {
-                    spreadsheetId: GOOGLE_SHEETS_DOC_ID,
-                    range: `${resource}!A:Z`,
-                }
-            );
+            const result = await getResourceRows(resource);
 
-            return processSheet(response.result.values);
+            return {
+                data: result.data,
+                total: result.total,
+            };
         },
         getManyReference: () => Promise.resolve(null),
         getOne: () => Promise.resolve(null),
