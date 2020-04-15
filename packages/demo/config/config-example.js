0ef29a0b9f83c26986b80bda572261251f2e3fec
Luwangel <adrien.amoros@gmail.com>
2020-04-15 16:07:50 +0200

    Remove config-dev

---
+++ packages/demo/config/config-example.js
@@ -0,0 +1,10 @@
+(function() {
+    console.log('Loading dev config');
+
+    window.CONFIG = {
+        PAGE_TITLE: 'Demo React Admin & Google Spreadsheets',
+        GOOGLE_SHEETS_API_KEY: 'SECRET',
+        GOOGLE_SHEETS_CLIENT_ID: 'SECRET',
+        GOOGLE_SHEETS_CLIENT_SECRET: 'SECRET',
+    };
+})();
