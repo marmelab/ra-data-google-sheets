818e321238215c2b219fd34daf9dd61275973f36
Luwangel <adrien.amoros@gmail.com>
2020-04-15 15:08:45 +0200

    Allow to customize the spreadsheetId in the googleSheetsDataProvider

--- packages/ra-data-google-sheets/src/googleSheetsDataProvider.js
+++ packages/ra-data-google-sheets/src/googleSheetsDataProvider.js
@@ -1,72 +1,80 @@
 import { processSheet, processRow } from './formatDataForReactAdmin';
 import { processForm } from './formatDataForGoogleSheets';
 
-const GOOGLE_SHEETS_DOC_ID = '1cagTLWhyPnlFpgCcu19W4O0wBCzXxEyDb5yBK_jg-PU';
+export const googleSheetsDataProvider = (
+    spreadsheetId = '1cagTLWhyPnlFpgCcu19W4O0wBCzXxEyDb5yBK_jg-PU'
+) => {
+    const getResourceRows = async resource => {
+        const response = await window.gapi.client.sheets.spreadsheets.values.get(
+            {
+                spreadsheetId,
+                range: `${resource}`,
+            }
+        );
 
-const getResourceRows = async resource => {
-    const response = await window.gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: GOOGLE_SHEETS_DOC_ID,
-        range: `${resource}`,
-    });
+        return processSheet(response.result.values);
+    };
 
-    return processSheet(response.result.values);
-};
+    const getResourceHeaders = async resource => {
+        const response = await window.gapi.client.sheets.spreadsheets.values.get(
+            {
+                spreadsheetId,
+                range: `${resource}!A1:Z1`,
+            }
+        );
 
-const getResourceHeaders = async resource => {
-    const response = await window.gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: GOOGLE_SHEETS_DOC_ID,
-        range: `${resource}!A1:Z1`,
-    });
+        return processSheet(response.result.values).headers;
+    };
 
-    return processSheet(response.result.values).headers;
-};
+    const countResourceRows = async resource => {
+        const response = await window.gapi.client.sheets.spreadsheets.values.get(
+            {
+                spreadsheetId,
+                range: `${resource}!A1:A1000`,
+            }
+        );
 
-const countResourceRows = async resource => {
-    const response = await window.gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: GOOGLE_SHEETS_DOC_ID,
-        range: `${resource}!A1:A1000`,
-    });
+        return processSheet(response.result.values).total;
+    };
 
-    return processSheet(response.result.values).total;
-};
+    const appendResourceRow = async (resource, row) => {
+        return await window.gapi.client.sheets.spreadsheets.values.append({
+            spreadsheetId,
+            range: `${resource}!A1`, // Automatically added at the end
+            valueInputOption: 'USER_ENTERED',
+            resource: {
+                values: row,
+            },
+        });
+    };
 
-const appendResourceRow = async (resource, row) => {
-    return await window.gapi.client.sheets.spreadsheets.values.append({
-        spreadsheetId: GOOGLE_SHEETS_DOC_ID,
-        range: `${resource}!A1`, // Automatically added at the end
-        valueInputOption: 'USER_ENTERED',
-        resource: {
-            values: row,
-        },
-    });
-};
+    const getResourceRow = async (resource, id) => {
+        const headers = await getResourceHeaders(resource);
+        const rowNumber = parseInt(id, 10) + 1;
 
-const getResourceRow = async (resource, id) => {
-    const headers = await getResourceHeaders(resource);
-    const rowNumber = parseInt(id, 10) + 1;
+        const response = await window.gapi.client.sheets.spreadsheets.values.get(
+            {
+                spreadsheetId,
+                range: `${resource}!A${rowNumber}:Z${rowNumber}`,
+            }
+        );
 
-    const response = await window.gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: GOOGLE_SHEETS_DOC_ID,
-        range: `${resource}!A${rowNumber}:Z${rowNumber}`,
-    });
+        return processRow(headers, response.result.values);
+    };
 
-    return processRow(headers, response.result.values);
-};
+    const updateResourceRow = async (resource, id, row) => {
+        const rowNumber = parseInt(id, 10) + 1;
 
-const updateResourceRow = async (resource, id, row) => {
-    const rowNumber = parseInt(id, 10) + 1;
+        return await window.gapi.client.sheets.spreadsheets.values.update({
+            spreadsheetId,
+            range: `${resource}!A${rowNumber}`,
+            valueInputOption: 'USER_ENTERED',
+            resource: {
+                values: row,
+            },
+        });
+    };
 
-    return await window.gapi.client.sheets.spreadsheets.values.update({
-        spreadsheetId: GOOGLE_SHEETS_DOC_ID,
-        range: `${resource}!A${rowNumber}`,
-        valueInputOption: 'USER_ENTERED',
-        resource: {
-            values: row,
-        },
-    });
-};
-
-export const googleSheetsDataProvider = () => {
     return {
         create: async (resource, { data: formData }) => {
             const headers = await getResourceHeaders(resource);
